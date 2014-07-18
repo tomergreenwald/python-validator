fc6e98286d6a916dcedbbad2cbf53e1317a8ac2f
Tomer Greenwald <tomerg@gmail.com>
2014-07-18 09:40:18 +0300

    Handled more assignment cases

--- validator/visitors/ClassVisitor.py
+++ validator/visitors/ClassVisitor.py
@@ -19,6 +19,23 @@ class ClassVisitor(NodeVisitor):
         self.generic_visit(node)
 
     def visit_Assign(self, node):
-        if node.targets[0].value.id is 'self':
-            self.current_class.attributes[node.targets[0].attr] = AttributeVisitor(self.class_dict).visit(node.value)
-        #FIXME: what do we do if the assignment is not to self?
+        #we do not handle multiple targets since we assume that the ast was preprocessed and every multiple assignment
+        #was split into separate assignments with temporary variables.
+        node_targets = node.targets[0]
+        if hasattr(node_targets, 'value'):
+            target = node_targets.value.id
+            attr = node_targets.attr
+            value = AttributeVisitor(self.class_dict).visit(node.value)
+            if target is 'self':
+                self.current_class.attributes[attr] = value
+                #TODO: register assignment to 'self' with Aviel's data structure
+            else:
+                pass
+                #TODO: register assignment to 'target' with Aviel's data structure. in this case the assignment is to a
+                #paramater sent to the __init__ method
+        else:
+            #target = global variables
+            attr = node_targets.id
+            value = AttributeVisitor(self.class_dict).visit(node.value)
+            i = 1
+            #TODO: register assignment to 'global' with Aviel's data structure.
