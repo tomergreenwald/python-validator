6829a6f69d9a440a9b66a46db58a93332b526cc2
Tomer Greenwald <tomerg@gmail.com>
2014-07-28 18:45:03 +0300

    Added code to parse

--- parsing.py
+++ parsing.py
@@ -224,6 +224,20 @@ classes = {}
 functions = {}
 varz = {}
 
+code = """
+
+class A(object):
+    def __init__(self, param):
+        self.x = 5
+        a = 'a'
+        b = param
+
+class B(A):
+    def __init__(self):
+        super(B, self).__init__()
+        A.__init__(self)
+a = A()
+"""
 ast_tree = ast.parse(code)
 visitor = ProgramVisitor()
 visitor.visit(ast_tree)
