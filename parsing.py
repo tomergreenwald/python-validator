100579381039a3c2371f7523cd5c27e6ebf66d77
Tomer Greenwald <tomerg@gmail.com>
2014-08-01 11:21:44 +0300

    Ignored not-yet supported assignments

--- parsing.py
+++ parsing.py
@@ -13,12 +13,12 @@ code = """
 #b = None
 v = 5
 v2 = 'a'
-v2.v = v
+#v2.v = v
 v3 = ''
 v3.a = 5
 v3.b = 'b'
-v3.v = v
-v2 = v3
+#v3.v = v
+#v2 = v3
 """
 ast_tree = ast.parse(code)
 visitor = ProgramVisitor()
