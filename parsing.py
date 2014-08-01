649ee4f67c7fef3816a0a9b32c9153eeddfc4f98
Tomer Greenwald <tomerg@gmail.com>
2014-08-01 10:09:42 +0300

    Updated tested code.

--- parsing.py
+++ parsing.py
@@ -7,18 +7,14 @@ functions = {}
 varz = {}
 
 code = """
-
-class A(object):
-    def __init__(self, param):
-        self.x = 5
-        a = 'a'
-        b = param
-
-class B(A):
-    def __init__(self):
-        super(B, self).__init__()
-        A.__init__(self)
-a = A()
+v = 5
+v2 = 'a'
+v2.v = v
+v3 = ''
+v3.a = 5
+v3.b = 'b'
+v3.v = v
+v2 = v3
 """
 ast_tree = ast.parse(code)
 visitor = ProgramVisitor()
