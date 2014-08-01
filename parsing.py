acc23c6dbfd2f756d348b19cdcd3d472188bccea
Tomer Greenwald <tomerg@gmail.com>
2014-08-01 16:38:12 +0300

    Enabled assignments from var to var

--- parsing.py
+++ parsing.py
@@ -14,12 +14,12 @@ b = False
 c = None
 v = 5
 v2 = 'a'
-#v2.v = v
+v2.v = v
 v3 = ''
 v3.a = 5
 v3.b = 'b'
-#v3.v = v
-#v2 = v3
+v3.v = v
+v2 = v3
 """
 ast_tree = ast.parse(code)
 visitor = ProgramVisitor()
