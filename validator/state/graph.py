e40d267cb7e914af8d710db3cf9a4fce859e42b5
Aviel Atias <avielatias@gmail.com>
2014-08-13 22:54:45 +0300

    step_father changes. changed it to all_parents. added Edge class

--- validator/state/graph.py
+++ validator/state/graph.py
@@ -2,36 +2,45 @@ import logging
 from lattice import LatticeElement as LE
 logging.basicConfig(level = logging.DEBUG)
 
+class GraphEdge(object):
+    def __init__(self, label = None, son = None, par = -1, know = None):
+        self.label = label
+        self.son = son
+        self.parent = par
+        self.knowledge = know if know is not None else LE(LE.L_MUST_HAVE)
+    
+    def __repr__(self):
+        return '%03d - > %03d\tlabel\t%s\tknowledge\t%s' \
+               %(self.son, self.parent, self.label, self.knowledge)
+
 class GraphVertex(object):
     def __init__(self, ind, label):
-        self.bio_label = label
         self.ind = ind
-        self.bio_parent = -1
-        self.step_parents = set()
         self.constant = -1
+        self.bio_edge = GraphEdge(label, son = ind, par = -1)
         self.sons = dict()
+        self.all_parents = dict()
         self.knowledge = LE(LE.L_MUST_HAVE)
     
     def __repr__(self):
-        return 'bparent\t%d\tlabel\t%s\tconst\t%d\n' %(self.bio_parent, self.bio_label, self.constant) + \
-               'knowledge\t%s\n' %(self.knowledge.get_element_name()) + \
-               'sons\t%s\n' %(self.sons.items()) + \
-               'sparents\t%s' %(self.step_parents.items())
+        return 'const\t%d\tbio edge\t%s\tknowledge\t%s\n' %(self.constant, self.bio_edge, self.knowledge) + \
+               'sons:\n%s\n' %('\n'.join(['\t%s' %x for x in self.sons.values()])) + \
+               'parents:\n%s' %('\n'.join(['\t%s' %x for x in self.all_parents.values()])) + \
 
 class Graph(object):
     """
     Graph class. each vertex is labelled by a non negative integer, and has:
         * single biological parent
         * possibly many step fathers
-        * sons (inverse of step_parents)
+        * sons (inverse of all_parents)
         * index of a constant it equals to
         * the label of the edge connecting it to its father
         * each vertex has an element from the lattice
     There are constants, but we save only one instance for each type
     
     the following invariants must always hold:
-        par = self.vertices[son].bio_parent => par in self.vertices[son].step_parents
-        par in self.vertices[son].step_parents => self.vertices[par].sons[self.vertices[son].bio_label] = son
+        par = self.vertices[son].bio_parent => par in self.vertices[son].all_parents
+        par in self.vertices[son].all_parents => self.vertices[par].sons[self.vertices[son].bio_label] = son
         v in self.vertices.values() => v.bio_parent >= 0 | v.constant >= 0 | v.is_top (this should be maintained by caller)
         the graph is a DAG
     """
@@ -78,7 +87,7 @@ class Graph(object):
         if not self.vertices.has_key(son) or not self.vertices.has_key(par):
             raise KeyError()
             
-        if self.vertices.step_parents:
+        if self.vertices.all_parents:
             raise Exception("[make_parent] making parent of son who already has parents...")
         """
         old_parent = self.vertices[son].bio_parent
@@ -86,24 +95,28 @@ class Graph(object):
             self.vertices[old_parent].sons.pop(self.vertices[son].bio_label)
         """
         
-        self.vertices[son].bio_parent = par
-        self.vertices[son].step_parents.add(par)
-        self.vertices[par].sons[self.vertices[son].bio_label] = son
+        bedge = self.vertices[son].bio_edge
+        bedge.parent = par
+        # now biological edge contains all the information
+        self.vertices[son].all_parents[bedge.label] = bedge
+        self.vertices[par].sons[bedge.label] = bedge
     
     def get_parent(self, v):
         """
         get biological parent index for vertex
         """
-        return self.vertices[v].bio_parent
+        return self.vertices[v].bio_edge.parent
     
     def is_top(self, v):
         """
+        TODO rewrite this function
         returns true if a var is TOP
         """
         return self.vertices[v].knowledge.val == LE.L_TOP
     
     def set_top(self, v):
         """
+        TODO rewrite this function
         set a var to be TOP
         """
         self.vertices[v].knowledge.val = LE.L_TOP
@@ -111,6 +124,7 @@ class Graph(object):
     
     def get_knowledge(self, v):
         """
+        TODO rewrite this function
         returns knowledge of vertex
         """
         return self.vertices[v].knowledge
@@ -129,8 +143,8 @@ class Graph(object):
             if const_ind < 0 and self.vertices[cur_v].constant >= 0:
                 const_ind = self.vertices[cur_v].constant
                 break
-            path_to_const.append(self.vertices[cur_v].bio_label)
-            cur_v = self.vertices[cur_v].bio_parent
+            path_to_const.append(self.vertices[cur_v].bio_edge.label)
+            cur_v = self.vertices[cur_v].bio_edge.parent
         
         if const_ind < 0:
             return None
@@ -176,11 +190,15 @@ class Graph(object):
         vertex_const = self.get_rooted_const(vertex_ind)
         
         for (lbl, v) in self.vertices[vertex_ind].sons.items():
-            if self.vertices[v].bio_parent != vertex_ind:
+            # remove from all_parents
+            self.vertices[v].all_parents.pop(lbl)
+            
+            if self.vertices[v].bio_edge.parent != vertex_ind:
                 # vertex belongs to another parent
-                # TODO remove from step_parents
                 continue
-            self.vertices[v].parent = -1
+                
+            self.vertices[v].bio_edge.parent = -1
+            self.vertices[v].bio_edge.label = None # just to be sure, doesnt really matter
             
             if self.vertices[v].constant < 0:
                 if vertex_const is not None:
@@ -210,10 +228,9 @@ class Graph(object):
         """
         self.unlink_vertex(vertex_ind)
         
-        par = self.vertices[vertex_ind].bio_parent
-        # TODO remove from step_parents also
-        if par >= 0:
-            self.vertices[par].sons.pop(self.vertices[vertex_ind].label)
+        for (lbl, e) in self.vertices[vertex_ind].all_parents.items():
+            self.vertices[e.parent].sons.pop(lbl)
+        
         self.vertices.pop(vertex_ind)
     
     def __repr__(self):
