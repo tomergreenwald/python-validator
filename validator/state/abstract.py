c1d451d64ffc0025a77f57bb0899203d4d40ead4
Aviel Atias <avielatias@gmail.com>
2014-08-18 22:25:53 +0300

    added TODO considering "verify" function discussed in email

--- validator/state/abstract.py
+++ validator/state/abstract.py
@@ -7,6 +7,8 @@ from lattice import LatticeElement as LE
 
 """
 TODO when querying for some var and the result is not L_MUST_HAVE, consider changing the full path to L_MUST_HAVE
+the logic should be: query the var from highest parent. any time an exception is raised, report it, and set this var 
+to L_MUST_HAVE or TOP
 """
 
 class AbstractState(object):
