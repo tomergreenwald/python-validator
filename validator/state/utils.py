99a866e386dfbb272234c5f00436e2ef9c01dc7d
Aviel Atias <avielatias@gmail.com>
2014-08-22 09:57:15 +0300

    modified the meaning of "basename" to contain scope if var is main var if a father doesnt exist, it is '' instead of None

--- validator/state/utils.py
+++ validator/state/utils.py
@@ -7,17 +7,18 @@ def var_to_father(var_name):
     base_name = var_name[last_sep:]
     last_dot = base_name.rfind('.')
     if last_dot < 0:
-        return None
+        return ''
     father = base_name[:last_dot]
     return scope + father
 
 def var_to_basename(var_name):
     """
     converts var of form: f!g!h!x.a.b to be b
+    and var of the form f!g!h!x to be unchanged
     """
-    last_sep = var_name.rfind('!') + 1
-    scope = var_name[:last_sep]
-    base_name = var_name[last_sep:]
-    last_dot = base_name.rfind('.')
-    return base_name[last_dot + 1:]
+    last_dot = var_name.rfind('.')
+    
+    if last_dot < 0:
+        return var_name
+    return var_name[last_dot + 1:]
     
