68c3f8239c89fe556d99a5da26cb40db85180fcb
oded <odedelbaz@gmail.com>
2014-09-25 14:24:16 +0300

    Added run by yourself!

---
+++ run_by_yourself.py
@@ -0,0 +1,23 @@
+__author__ = 'Oded'
+import sys
+import ast
+
+from validator.visitors.Visitors import ProgramVisitor
+from validator.simplers import simpler
+
+if __name__ == '__main__':
+    with file(sys.argv[1]) as f:
+        code = f.read()
+
+    print 'Orignal Code:'
+    print code
+    print
+    simple = simpler.make_simple(code)
+    print 'After simpler:'
+    print '=============='
+    print simple
+    print
+
+    ast_tree = ast.parse(simple)
+    visitor = ProgramVisitor()
+    visitor.visit(ast_tree)
