548efce47728bf01cc61997e32d0aa55fb01e94c
oded <odedelbaz@gmail.com>
2014-09-25 15:13:15 +0300

    pass example 1 :)

--- validator/visitors/Visitors.py
+++ validator/visitors/Visitors.py
@@ -268,7 +268,6 @@ class AssignVisitor(CallVisitor):
         temp_node = ast.Attribute(id=name, ctx=ast.Store())
         register_assignment(self.stack, self.abstract_state, temp_node, self.name)
 
-
     def visit_Subscript(self, node):
         if type(node.ctx) is ast.Load:
             try:
@@ -363,14 +362,12 @@ class AssignVisitor(CallVisitor):
 class ExprVisitor(CallVisitor):
     def __init__(self, stack, abstract_state, functions, classes):
         super(ExprVisitor, self).__init__(stack, abstract_state, functions, classes)
-    """
-    def visit_Expr(self, node):
-        # name = actual_var_name(self.stack, node.value.value.id) + "#" + node.value.attr
-        name = actual_var_name(self.stack, node.value.value.id) + "." + node.value.attr
+
+    def visit_Attribute(self, node):
+        name = actual_var_name(self.stack, node.value.id) + "." + node.attr
         print "Evaluatin expression - {name}".format(name=name)
-        errors = self.abstract_state.query(name, False)
+        errors = self.abstract_state.query(name)
         print errors
-    """
 
 
 class FunctionDefVisitor(ast.NodeVisitor):
