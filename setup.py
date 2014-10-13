08191fdb0dc7bec23c4f8425dc7c043acb58eb56
oded elbaz <odedelbaz@gmail.com>
2014-10-13 15:31:45 +0300

    Normal setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
-__author__ = 'Oded'
+from setuptools import setup
 
+setup(name='PythonValidator',
+      version='1.0',
+      description='Python Attributes Validator',
+      author='Tomer G, Aviel A, Oded E',
+      packages=['validator'],
 
-# Soon
+      install_requires=[
+          'astor',
+          'codegen'
+      ],
+
+      entry_points={'console_scripts': [
+        'validator-example = validator.examples:main',
+        'validate = validator.run_by_yourself:main'
+    ]},
+     )
