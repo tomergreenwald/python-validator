af67ed2f1580581c3b8add0fff71d8cfeced2d9d
Tomer Greenwald <tomerg@gmail.com>
2014-10-17 18:47:01 +0300

    Reformatted for PEP8

--- validator/state/lattice.py
+++ validator/state/lattice.py
@@ -1,5 +1,6 @@
 from state_exceptions import VerifierError, VerifierWarning
 
+
 class LatticeElement(object):
     L_BOTOM = -1
     L_MAY_HAVE = 0
@@ -7,41 +8,41 @@ class LatticeElement(object):
     L_MUST_NOT_HAVE = 2
     L_TOP = 3
     TOTAL_ATTR_TYPES = 4
-    
+
     def __init__(self, element):
         self.val = element
 
     def __eq__(self, other):
         return (isinstance(other, self.__class__)
-            and self.__dict__ == other.__dict__)
+                and self.__dict__ == other.__dict__)
 
     def __ne__(self, other):
         return not self.__eq__(other)
-    
+
     def get_element_name(self):
         for v in dir(self):
             if v.startswith('L_') and self.__getattribute__(v) == self.val:
                 return v
         return 'UNKNOWN_ELEMENT'
-    
+
     def __repr__(self):
         return self.get_element_name()
-    
+
     def get_element_enum(self):
         """
         we want to associate each element with a number
         """
         return self.val
-    
+
     def is_legal(self):
         if self.val == LatticeElement.L_MAY_HAVE:
             raise VerifierWarning("Attribute might not exist")
         elif self.val == LatticeElement.L_MUST_NOT_HAVE or self.val == LatticeElement.L_BOTOM:
             raise VerifierError("Attribute does not exist")
-    
+
     def inplace_lub(self, p):
         self.val = self.lub(self, p).val
-    
+
     @staticmethod
     def lub(p0, p1):
         """
@@ -64,18 +65,17 @@ class LatticeElement(object):
         """
         if p0 == p1:
             return LatticeElement(p0.val)
-        
+
         if p0.val == LatticeElement.L_BOTOM:
             return LatticeElement(p1.val)
         if p1.val == LatticeElement.L_BOTOM:
             return LatticeElement(p0.val)
-        
+
         if p0.val == LatticeElement.L_TOP or p1.val == LatticeElement.L_TOP:
             return LatticeElement(LatticeElement.L_TOP)
-        
+
         if p0.val == LatticeElement.L_MAY_HAVE or p1.val == LatticeElement.L_MAY_HAVE:
             return LatticeElement(LatticeElement.L_MAY_HAVE)
-        
+
         # This is the case for this lattice. if the lattice is changed, this function should be updated more carefully
         return LatticeElement(LatticeElement.L_MAY_HAVE)
-        
--- validator/state/utils.py
+++ validator/state/utils.py
@@ -1,60 +1,78 @@
-import copy
 import types
 import ast
 
 TOP_MAGIC_NAME = "MAKE_THIS_VAR_AS_TOP"
 # TODO add more known functions, for floats, strings, and non-boolean functions
-INT_FUNCS = set([(int, x) for x in ['__add__', '__and__', '__cmp__', '__div__', \
-                                    '__divmod__', '__lshift__', '__mod__', '__mul__', \
-                                    '__or__', '__pow__', '__radd__', '__rand__', \
-                                    '__rdiv__', '__rdivmod__', '__rfloordiv__', \
-                                    '__rlshift__', '__rmod__', '__rmul__', '__ror__', \
-                                    '__rpow__', '__rrshift__', '__rshift__', \
-                                    '__rsub__', '__rtruediv__', '__rxor__', \
+INT_FUNCS = set([(int, x) for x in ['__add__', '__and__', '__cmp__', '__div__',
+                                    '__divmod__', '__lshift__', '__mod__', '__mul__',
+                                    '__or__', '__pow__', '__radd__', '__rand__',
+                                    '__rdiv__', '__rdivmod__', '__rfloordiv__',
+                                    '__rlshift__', '__rmod__', '__rmul__', '__ror__',
+                                    '__rpow__', '__rrshift__', '__rshift__',
+                                    '__rsub__', '__rtruediv__', '__rxor__',
                                     '__sub__', '__truediv__', '__xor__']]) | \
             set([(str, x) for x in ['__len__', '__sizeof__', '__hash__']])
-            
-BOOL_FUNCS = set([(str, x) for x in ['isalnum', 'isalpha', 'isdigit', 'islower', \
+
+BOOL_FUNCS = set([(str, x) for x in ['isalnum', 'isalpha', 'isdigit', 'islower',
                                      'isspace', 'istitle', 'isupper']]) | \
              set([(int, x) for x in ['__nonzero']])
+
+
 class TopFunction(object):
     index = -1
+
     @staticmethod
     def get(num_args):
         TopFunction.index += 1
-        return ast.parse('\ndef func_%s_%d(%s):\n    return %s_%d\n' %(TOP_MAGIC_NAME, TopFunction.index, ', '.join(['arg%d' %j for j in xrange(num_args)]), TOP_MAGIC_NAME, TopFunction.index)).body[0]
+        return ast.parse('\ndef func_%s_%d(%s):\n    return %s_%d\n' % (
+        TOP_MAGIC_NAME, TopFunction.index, ', '.join(['arg%d' % j for j in xrange(num_args)]), TOP_MAGIC_NAME,
+        TopFunction.index)).body[0]
+
 
 class IntFunction(object):
     index = -1
+
     @staticmethod
     def get(num_args):
         IntFunction.index += 1
         return ast.parse('\ndef func_%s_%d(%s):\n    tmptmptmpvarvarvar = 0\n    return tmptmptmpvarvarvar\n' \
-                         %('MAKE_THIS_AS_INT', IntFunction.index, \
-                         ', '.join(['arg%d' %j for j in xrange(num_args)]))).body[0]
+                         % ('MAKE_THIS_AS_INT', IntFunction.index, \
+                            ', '.join(['arg%d' % j for j in xrange(num_args)]))).body[0]
+
 
 class BoolFunction(object):
     index = -1
+
     @staticmethod
     def get(num_args):
         BoolFunction.index += 1
         return ast.parse('\ndef func_%s_%d(%s):\n    tmptmptmpvarvarvar = False\n    return tmptmptmpvarvarvar\n' \
-                         %('MAKE_THIS_AS_BOOL', BoolFunction.index, \
-                         ', '.join(['arg%d' %j for j in xrange(num_args)]))).body[0]
-        
-PRIMITIVES = set([eval('types.%s' %x) for x in dir(types) if x.endswith('Type')])
+                         % ('MAKE_THIS_AS_BOOL', BoolFunction.index, \
+                            ', '.join(['arg%d' % j for j in xrange(num_args)]))).body[0]
+
+
+PRIMITIVES = set([eval('types.%s' % x) for x in dir(types) if x.endswith('Type')])
+
 
 def tmp_f():
     pass
+
+
 tmp_int = 5
+
+
 class tmp_T(object):
     def f(self):
         pass
+
+
 tmp_str = '5'
 CALLABLES = set(map(type, [tmp_f, tmp_int.__add__, tmp_T().f, tmp_str.isalpha]))
 
+
 def is_top_func(f):
     return TOP_MAGIC_NAME in f.name
+
 
 def is_primitive(obj):
     # sometime copy fails (for example when we create an empty class and look at its __init__ method
@@ -65,20 +83,22 @@ def is_primitive(obj):
     except:
         return True
     """
-    
+
     try:
         obj.fdsfsdfdsfdsfdsfdsfdsfds = 5
         del obj.fdsfsdfdsfdsfdsfdsfdsfds
         return False
     except:
         return True
-        
+
+
 def is_callable(obj):
     """
     check if obj is a function
     """
     return type(obj) in CALLABLES
-        
+
+
 def var_to_father(var_name):
     """
     converts var of form: f#g#h#x.a.b to be f#g#h#x.a
@@ -92,16 +112,18 @@ def var_to_father(var_name):
     father = base_name[:last_dot]
     return scope + father
 
+
 def var_to_basename(var_name):
     """
     converts var of form: f#g#h#x.a.b to be b
     and var of the form f#g#h#x to be unchanged
     """
     last_dot = var_name.rfind('.')
-    
+
     if last_dot < 0:
         return var_name
     return var_name[last_dot + 1:]
+
 
 class BasicMutableClass(object):
     pass
